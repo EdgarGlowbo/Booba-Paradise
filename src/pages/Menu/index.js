97f3ee7185c307f20b6c9a83b3518a143cfd42e2
EdgarGlowbo <efqr811@gmail.com>
2023-07-05 14:34:32 -0700

    Finished Menu screen header

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import styles from "./styles";
+import { ImageBackground } from "react-native";
+import { useFonts } from "expo-font";
+import { Damion_400Regular } from "@expo-google-fonts/damion";
+import { Poppins_400Regular } from "@expo-google-fonts/poppins";
+import { Poppins_600SemiBold } from "@expo-google-fonts/poppins";
+import * as SplashScreen from "expo-splash-screen";
 
 const Menu = () => {
-  return <Text style={styles.container}>Menu</Text>;
+  const [fontsLoaded] = useFonts({
+    Damion_400Regular,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <ImageBackground
+          source={require("../../../assets/wavyBackground.jpg")}
+          style={styles.backgroundImg}
+        >
+          <View style={styles.headerTextContainer}>
+            <Text style={styles.headerTitle}>Menú</Text>
+            <Text style={styles.headerDesc}>
+              Un booba frappé, un refresher, waffles, lo que sea que se te
+              antoje, hay suficiente para escoger.
+            </Text>
+          </View>
+        </ImageBackground>
+      </View>
+    </View>
+  );
 };
 
 export default Menu;
--- src/pages/Menu/styles.js
+++ src/pages/Menu/styles.js
@@ -1,9 +1,46 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+import { colors, fonts } from "../../variables";
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
-    // justifyContent: "center",
+  },
+  header: {
+    width: Dimensions.get("screen").width,
+    height: Dimensions.get("screen").height * (1 / 2),
+    overflow: "hidden",
+  },
+  backgroundImg: {
+    flex: 1,
+    paddingTop: 74,
+    // backgroundPosition: "center",
+    resizeMethod: "resize",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  headerTextContainer: {
+    margin: 16,
+    rowGap: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerTitle: {
+    fontFamily: fonts.headings,
+    fontSize: 32,
+    color: colors.background,
+  },
+  headerDesc: {
+    fontFamily: fonts.body,
+    fontSize: 18,
+    color: colors.background,
+    textAlign: "center",
+  },
+  headerImg: {
+    position: "relative",
+    height: 600,
+    width: 900,
+    resizeMode: "cover",
+    bottom: -130,
   },
 });
 
