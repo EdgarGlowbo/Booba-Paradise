ba64374fa75bcc5195f194b852f3261088315364
EdgarGlowbo <efqr811@gmail.com>
2023-07-12 18:22:18 -0700

    @react-google-maps/api library works. useAxios is called every render

--- src/pages/StoreLocator/index.js
+++ src/pages/StoreLocator/index.js
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import styles from "./styles";
 // import { MapView, Marker } from "react-native-maps";
+import { useLoadScript, GoogleMap, Marker } from "@react-google-maps/api";
+import { GOOGLE_MAPS_API_KEY } from "../../apis/apiKey";
+import useAxios from "../../hooks/useAxios";
 import axiosInstance from "../../apis/api_instance";
+
 const StoreLocator = () => {
-  // const { response } = useAxios({
-  //   axiosInstance: axiosInstance,
-  //   method: "GET",
-  //   url: "/location",
-  //   requestConfig: {
-  //     headers: {
-  //       "Content-Language": "en-US",
-  //     },
-  //   },
-  // });
-  const [origin, setOrigin] = useState({});
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+  });
+
+  const { response } = useAxios({
+    axiosInstance: axiosInstance,
+    method: "GET",
+    url: "location",
+    requestConfig: {
+      headers: {
+        "Content-Language": "en-US",
+      },
+    },
+  });
+  const center = useMemo(() => {
+    if (response) {
+      return {
+        lat: parseFloat(response[0].latitude),
+        lng: parseFloat(response[0].longitude),
+      };
+    }
+  }, [response]);
   return (
     <View style={styles.container}>
-      {/* {response && (
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: response.latitude,
-            longitude: response.longitude,
-            latitudeDelta: 0.09,
-            longitudeDelta: 0.04,
-          }}
-        >
-          <Marker
-            coordinate={{
-              latitude: response.latitude,
-              longitude: response.longitude,
-            }}
-          />
-        </MapView>
-      )} */}
+      {isLoaded && response && (
+        <GoogleMap zoom={17} center={center} mapContainerStyle={styles.map}>
+          <Marker position={center} />
+        </GoogleMap>
+      )}
     </View>
   );
 };
